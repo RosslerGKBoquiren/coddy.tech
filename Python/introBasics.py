c0afd7fcc23bfca38460b16ac55058479bb14843
RosslerGKBoquiren <142700435+RosslerGKBoquiren@users.noreply.github.com>
2023-11-10 02:03:06 -0500

    Add files via upload

--- Python/introBasics.py
+++ Python/introBasics.py
@@ -30,3 +30,20 @@ n3 = n1 > n2
 b1 = (3 > 1) and (6 < 10)
 b2 = not (6 > 10 or 10 < 8)  
 b3 = b1 or b2
+# Logical Operators Part 2
+b1 = 1
+b2 = 1
+b3 = not (b1 + b2) < (b1 * b2)
+
+# Challenge#1
+# You are given a code, initialize the variables a and b so that c will hold 24.
+a = 2
+b = 12
+c = b * a
+# Challenge#2
+# You are given a code, replace the question marks of the variables b1, b2  and b3 so that b4 will hold True.
+b1 = (1 < 2) and (2 > 1)
+b2 = (2 > 1) or (1 > 0)
+b3 = (2 < 1) and (1 > 2) 
+# Don't change the line below
+b4 = b1 and b2 and (not b3)
