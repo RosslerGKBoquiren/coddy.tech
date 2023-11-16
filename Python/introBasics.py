0bc9b3c1a2ba13685fef6ae43cd0d3b3d3f0797a
RosslerGKBoquiren <142700435+RosslerGKBoquiren@users.noreply.github.com>
2023-11-16 12:06:44 -0500

    Add files via upload

--- Python/introBasics.py
+++ Python/introBasics.py
@@ -147,3 +147,11 @@ number = float(input())
 while number >= 3.5:
     number /= 2
 print(number)
+
+# break
+# You are given a code that prints the numbers from 1 to 10 (including).
+# Your task is to add if and break statements so that only the numbers from 1 to 5 will be printed, the loop will exit before printing the numbers from 6 to 10.
+for i in range(1, 11):
+    if i == 6:
+        break
+    print(i)
