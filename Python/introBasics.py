1992dd40afd6d38f81e8ffd0be935412327f84c2
RosslerGKBoquiren <142700435+RosslerGKBoquiren@users.noreply.github.com>
2023-11-16 12:04:34 -0500

    Add files via upload

--- Python/introBasics.py
+++ Python/introBasics.py
@@ -139,3 +139,11 @@ for i in range (3, 28):
 # Print Hello,  and the stored variable in the end.
 name = input('')
 print(f"Hello, {name}")
+
+# Write a program that gets one input, float number.
+# Use a while loop to divide the input by 2 as long as the number is bigger or equal to 3.5.
+# Print the first number that is smaller than 3.5.
+number = float(input())
+while number >= 3.5:
+    number /= 2
+print(number)
