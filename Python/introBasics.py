44ef63a370450f4c90d666a573043b58a9cc220d
RosslerGKBoquiren <142700435+RosslerGKBoquiren@users.noreply.github.com>
2023-11-16 12:12:51 -0500

    Add files via upload

--- Python/introBasics.py
+++ Python/introBasics.py
@@ -155,3 +155,11 @@ for i in range(1, 11):
     if i == 6:
         break
     print(i)
+
+# continue
+# You are given a code which prints the numbers from 1 to 20 (including).
+# Your task is to add if and continue statements so that only the even numbers will be printed (2, 4, 6, ...). 
+for i in range(1, 21):
+    if i % 2 == 1:
+        continue
+    print(i)
